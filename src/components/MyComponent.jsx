e80d4be9def329a0707d2aed0ebf5c64eb3ea4ba
“HTQun” <qh300702@gmail.com>
2025-05-28 14:45:40 +0700

    setState

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -6,10 +6,32 @@ class MyComponent extends React.Component {
         address: "Vn",
         Age: 22,
     };
+
+    handleClick(event) {
+        console.log("click me my button");
+        console.log("random ", Math.floor(Math.random() * 100 + 1));
+        this.setState({
+            name: "Quan huynh",
+            age: Math.floor(Math.random() * 100 + 1),
+        });
+    }
+
+    handleHover(event) {
+        // console.log("click me my button");
+        // console.log(event.target);
+    }
+
     render() {
         return (
             <div>
-                My name is {this.state.name} and i'm from {this.state.address}
+                <p>
+                    My name is {this.state.name} and i'm from{" "}
+                    {this.state.address}
+                </p>
+                <button onClick={(event) => this.handleClick(event)}>
+                    Click me
+                </button>
+                <button onMouseOver={this.handleHover}>Hover me</button>
             </div>
         );
     }
